89bbf43be4126991f329a8f68f40cfe170316a30
benber7 <90457184+benber7@users.noreply.github.com>
2023-03-07 09:04:23 +0100

    kommentarer

--- app.js
+++ app.js
@@ -21,19 +21,16 @@ app.set("views", path.join(__dirname, "./views/pages"))
 hbs.registerPartials(path.join(__dirname, "./views/partials"))
 
 
-// Dyrepark
-
-// Dette er koden til registrerDyr.html formen
-app.post("/settinn", (req, res) => {
-    let svar = req.body
-    settInnDyr(svar.navn, svar.fodselsdato, svar.vekt, svar.kjonn, svar.artID)
-    res.redirect("back")
+// Dette er hovedsiden, du søker bare på http://localhost:3000/ og får opp en html side
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"))
 })
 
+// Koden for dyr delen av oppgaven
 
 // Når du søker på http://localhost:3000/angi starter du denne koden
 app.get("/angi", (req, res) => {
-    // Jeg lager en variabel som inneholder henting av data fra databasen gondwana.db og henter ut alt fra tabellen dyr
+    // lager en variabel som inneholder henting av data fra databasen gondwana.db og henter ut alt fra tabellen dyr
     let dyr = dbDyr.prepare("SELECT * FROM dyr").all()
     let objekt = {dyr: dyr}
     // Hvis du er logget in blir du sendt til registrerDyr.html hvis ikke blir du sendt til login.html
@@ -46,7 +43,7 @@ app.get("/angi", (req, res) => {
 
 // Når du søker på http://localhost:3000/visdyr starter du denne koden
 app.get("/visdyr", (req, res) => {
-    // Jeg lager en variabel som inneholder henting av data fra databasen gondwana.db og henter ut alt fra tabellen dyr
+    // lager en variabel som inneholder henting av data fra databasen gondwana.db og henter ut alt fra tabellen dyr
     let dyr = dbDyr.prepare("SELECT * FROM dyr").all()
     let objekt = {dyr: dyr}
     res.render("dyr.hbs", objekt)
@@ -65,46 +62,50 @@ app.get("/visart", (req, res) => {
    res.render("art.hbs", objekt)
 })
 
+// Dette blir brukt på siden registrerDyr.html med <form action="/settinn" og den legger til data i dyr tabellen i gondwana.db
+app.post("/settinn", (req, res) => {
+    let svar = req.body
+    settInnDyr(svar.navn, svar.fodselsdato, svar.vekt, svar.kjonn, svar.artID)
+    res.redirect("back")
+})
 
-// Dette skjører når du trykker på slett knappen på siden dyr.hbs
+// Dette blir brukt på siden dyr.hbs med <form action="/Slettdyr" og den sletter dyr i tabellen dyr
 app.post("/Slettdyr", (req, res) => {
     let dyrid = req.body.dyrid;
     slettDyr(dyrid);
     res.redirect("back");
 });
 
-//
+// Dette blir brukt på siden dyr.hbs med <form action="/Endredyr" og den endrer dyren i dyr tabellen
 app.post("/Endredyr", (req, res) => {
     let svar = req.body
     endreDyr(svar.navn, svar.vekt, svar.kjonn, svar.fodselsdato, svar.artID, svar.dyrid)
     res.redirect("back");
 });
 
-// Dette funskjonen blir kjørt i /settinn som legger in data i dyr tabellen
+// Her er funksjonen som blir kjørt i app.post("/settinn"
 function settInnDyr(navn, fodselsdato, vekt, kjonn, artID) {
     let settInnDyr = dbDyr.prepare("INSERT INTO dyr (navn, fodselsdato, vekt, kjonn, artID) VALUES (?, ?, ?, ?, ?)")
     settInnDyr.run(navn, fodselsdato, vekt, kjonn, artID)
 }
 
-// Dette er funskjonen som blir kjørt i /Slettdyr, den setter in data i dyr tabellen
+// Her er funskjonen som blir kjørt i app.post("/Slettdyr"
 function slettDyr(dyrid) {
     let slettDyr = dbDyr.prepare("DELETE FROM dyr WHERE dyrid = ?");
     slettDyr.run(dyrid);
 }
 
-// 
+// Her er funksjonen som blir kjørt i app.post("/Endredyr"
 function endreDyr(navn, vekt, kjonn, fodselsdato, artID, dyrid) {
     let dyr = dbDyr.prepare("UPDATE dyr SET navn = ?, vekt = ?, kjonn = ?, fodselsdato = ?, artID = ? WHERE dyrid = ?");
     dyr.run(navn, vekt, kjonn, fodselsdato, artID, dyrid)
 }
-// Dyrepark
 
 
 
+// Koden for bruker delen av oppgaven
 
-// Pålogging
-
-//Disse to er veier til registrer og login siden, de her blir kjørt i knapper
+//Disse to er veier til registrer og login siden, de er koblet til hver sin knapp i navbaren på de fleste sidene
 app.get("/Public/registrer", (req, res) => {
     res.sendFile(path.join(__dirname, "/Public/registrer.html"))
 })
@@ -112,18 +113,15 @@ app.get("/Public/login", (req, res) => {
     res.sendFile(path.join(__dirname, "/Public/login.html"))
 })
 
-// Dette er hovedsiden, du søker bare på http://localhost:3000/ og får opp en html side
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/index.html"))
-})
-
-// Dette blir kjørt i login.htm
+// Koden som kjører når du trykker på login 
 app.post("/login", async (req, res) => {
     let login = req.body;
     // Henter ut data fra brukere.db, user
     let userData = dbFolk.prepare("SELECT * FROM user WHERE email = ?").get(login.email);
     
+    // Her bruker jeg await for å gi bcrypt.compare nok til til å sammen ligne passordet du skrev inn med hashen som ligger i databasen
     if(await bcrypt.compare(login.password, userData.hash)) {
+        // hvis passordet du skrev in er lik hashen blir du redirected til index.html og hvis de er ikke lik blir du redirected tilbake
         req.session.loggedin = true
         res.redirect("/")
     } else {
@@ -131,11 +129,13 @@ app.post("/login", async (req, res) => {
     }
 })
 
+// Når du tyrkker på logg ut kjører den her. Den gjør om din cookie session fra true til false og redirecter deg til index.html
 app.get("/logut", (req, res) => {
     req.session.loggedin = false;
     res.redirect("/");
 });
 
+// Når du trykker på profil knappen kjører den her, den sender deg til profil.html hvis du er logget inn og hvis ikke den sender deg til login.html
 app.get("/profil", (req, res) => {
     if(req.session.loggedin) {
         res.sendFile(path.join(__dirname, "/Public/profil.html"))
@@ -144,32 +144,30 @@ app.get("/profil", (req, res) => {
     }
 })
 
+/*
 app.post("/slettBruker", (req, res) => {
-    let id = req.body.id;
-    Slettbruker(id);
-    req.session.loggedin = false;
-    res.redirect("/");
 });
+*/
 
-// 
+// Etter du har skrevet inn dine opplysninger og trykker på registrer
 app.post(("/addUser"), async (req, res) => {
+    // henter ut hva du har skrevet
     let svar = req.body;
-
+    // omgjør passordet du skrev inn til hash
     let hash = await bcrypt.hash(svar.password, 10)
-    console.log(svar)
-    console.log(hash)
-
+    //console.log(svar)
+    //console.log(hash)
+    // sender alt til database og der etter sender deg tilbake
     dbFolk.prepare("INSERT INTO user (name, email, hash) VALUES (?, ?, ?)").run(svar.name, svar.email, hash)
-    
     res.redirect("back")    
 })
+
 /*
 function Slettbruker(id) {
     let Slettbruker = dbFolk.prepare("DELETE FROM user WHERE id = ?");
     Slettbruker.run(id);
 }
 */
-// Pålogging
 
 app.listen("3000", () => {
     console.log("Server listening at http://localhost:3000")
